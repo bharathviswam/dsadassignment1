802cfcac807224a9dcc744064de769ec551a3e4b
bharathviswam <32975209+bharathviswam@users.noreply.github.com>
2020-12-24 08:28:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,7 @@ maxDeathYear = 10
 leastBirthYear = 2100
 
 def maxPop(arrayOfDictPerson):
-    print(leastBirthYear)
-    print(maxDeathYear)
     population_changes = [0 for _ in range(leastBirthYear, maxDeathYear+1)]
-    print(len(population_changes))
     for person in arrayOfDictPerson:
         population_changes[int(person.dob.split('-')[2]) - leastBirthYear] += 1
         if person.dod != '':
@@ -32,7 +29,6 @@ def maxPop(arrayOfDictPerson):
     max_population = 0
     max_population_index = 0
     population = 0
-    print(population_changes)
     for index, population_change in enumerate(population_changes):
         population += population_change
         if population > max_population:
@@ -43,10 +39,7 @@ def maxPop(arrayOfDictPerson):
 
 
 def minPop(arrayOfDictPerson):
-    print(leastBirthYear)
-    print(maxDeathYear)
     population_changes = [0 for _ in range(leastBirthYear, maxDeathYear+1)]
-    print(len(population_changes))
     for person in arrayOfDictPerson:
         population_changes[int(person.dob.split('-')[2]) - leastBirthYear] += 1
         if person.dod != '':
@@ -99,92 +92,109 @@ def maxDeath(arrayOfDeath):
 
 
 def printOutput(dict):
-    arrayOfDictPerson = dict[0]
-    arrayOfBirth = dict[1]
-    arrayOfDeath = dict[2]
-    recordCount = dict[3]
-    readPrompts = open('promptsPS21.txt','r')
-    for line in readPrompts:
-        line = line.strip()
-        rowSplit = line.split(':')
-        if rowSplit[0] == 'bornIn':
-            bornInInput = int(rowSplit[1].strip())
-            bornCount = countBorn(arrayOfDictPerson,bornInInput)
-        if rowSplit[0] == 'diedIn':
-            diedInInput = int(rowSplit[1].strip())
-            diedCount = countDied(arrayOfDictPerson,diedInInput)
-        if rowSplit[0] == 'maxPopulation':
-            arrMaxYearCount = maxPop(arrayOfDictPerson)
-        if rowSplit[0] == 'minPopulation':
-            arrMinYearCount = minPop(arrayOfDictPerson)
-        if rowSplit[0] == 'maxBirth':
-            maxBirthYear = maxBirth(arrayOfBirth)
-        if rowSplit[0] == 'maxDeath':
-            maxDeathYear = maxDeath(arrayOfDeath)
+    try:
+        arrayOfDictPerson = dict[0]
+        arrayOfBirth = dict[1]
+        arrayOfDeath = dict[2]
+        recordCount = dict[3]
+        try:
+            readPrompts = open('promptsPS21.txt','r')
+        except:
+            print("File promptsPS21.txt doesnot exist")
+        for line in readPrompts:
+            line = line.strip()
+            rowSplit = line.split(':')
+            if rowSplit[0] == 'bornIn':
+                bornInInput = int(rowSplit[1].strip())
+                bornCount = countBorn(arrayOfDictPerson,bornInInput)
+            if rowSplit[0] == 'diedIn':
+                diedInInput = int(rowSplit[1].strip())
+                diedCount = countDied(arrayOfDictPerson,diedInInput)
+            if rowSplit[0] == 'maxPopulation':
+                arrMaxYearCount = maxPop(arrayOfDictPerson)
+            if rowSplit[0] == 'minPopulation':
+                arrMinYearCount = minPop(arrayOfDictPerson)
+            if rowSplit[0] == 'maxBirth':
+                maxBirthYear = maxBirth(arrayOfBirth)
+            if rowSplit[0] == 'maxDeath':
+                maxDeathYear = maxDeath(arrayOfDeath)
 
-    writeOutput = open("outputPS21.txt", "w")   #w will create file if it does not exist
-    recordsCaptured = str(recordCount) + " records captured\n"   #need to complete 
-    writeOutput.write(recordsCaptured)
-    bornInText = "No. of people born in "+ str(bornInInput) +": "+ str(bornCount)+"\n"
-    writeOutput.write(bornInText)
-    diedInText = "No. of people died in "+ str(diedInInput) +": "+ str(diedCount)+"\n"
-    writeOutput.write(diedInText)
-    maxPopText = "Maximum population was in year "+ str(arrMaxYearCount[0])+" with "+str(arrMaxYearCount[1])+" people alive.\n"
-    writeOutput.write(maxPopText)
-    minPopText = "Minimum population was in year "+ str(arrMinYearCount[0])+" with "+str(arrMinYearCount[1])+" people alive.\n"
-    writeOutput.write(minPopText)
-    maxBirthText = "Maximum births were in year "+ str(maxBirthYear[0])+" with "+str(maxBirthYear[1])+" people born.\n"
-    writeOutput.write(maxBirthText)
-    maxDeathText = "Maximum deaths were in year "+ str(maxDeathYear[0])+" with "+str(maxDeathYear[1])+" people dead.\n"
-    writeOutput.write(maxDeathText)
-    writeOutput.close()
+        writeOutput = open("outputPS21.txt", "w")   #w will create file if it does not exist
+        if 'recordCount' in locals():
+            recordsCaptured = str(recordCount) + " records captured\n"   #need to complete 
+            writeOutput.write(recordsCaptured)
+        if 'bornInInput' in locals():
+            bornInText = "No. of people born in "+ str(bornInInput) +": "+ str(bornCount)+"\n"
+            writeOutput.write(bornInText)
+        if 'diedInInput' in locals():
+            diedInText = "No. of people died in "+ str(diedInInput) +": "+ str(diedCount)+"\n"
+            writeOutput.write(diedInText)
+        if 'arrMaxYearCount' in locals():
+            maxPopText = "Maximum population was in year "+ str(arrMaxYearCount[0])+" with "+str(arrMaxYearCount[1])+" people alive.\n"
+            writeOutput.write(maxPopText)
+        if 'arrMinYearCount' in locals():
+            minPopText = "Minimum population was in year "+ str(arrMinYearCount[0])+" with "+str(arrMinYearCount[1])+" people alive.\n"
+            writeOutput.write(minPopText)
+        if 'maxBirthYear' in locals():
+            maxBirthText = "Maximum births were in year "+ str(maxBirthYear[0])+" with "+str(maxBirthYear[1])+" people born.\n"
+            writeOutput.write(maxBirthText)
+        if 'maxDeathYear' in locals():
+            maxDeathText = "Maximum deaths were in year "+ str(maxDeathYear[0])+" with "+str(maxDeathYear[1])+" people dead.\n"
+            writeOutput.write(maxDeathText)
+        writeOutput.close()
+    except:
+        print("Something went wrong while writing output")
 
 def readInputData():
-    f = open('inputPS21.txt','r')
-    recordCount = 0
-    arrayOfDictPerson = []
-    arrayOfBirth = []
-    arrayOfDeath = []
-    global leastBirthYear 
-    leastBirthYear = 2100
-    global maxDeathYear
-    maxDeathYear = 0
-    for line in f:
-        recordCount+=1
-        line = line.strip()
-        rowSplit = line.split(',')
-        print(rowSplit[2].split('-')[2])
-        person = dictPerson(rowSplit[0],rowSplit[1],rowSplit[2],rowSplit[3])
-        arrayOfDictPerson.append(person)
-        birthFlag = 0
-        for birthObj in arrayOfBirth:
-            if birthObj.year == rowSplit[2].split('-')[2]:
-                birthObj.birthCount += 1
-                birthFlag = 1
-                print(leastBirthYear)
+    try:
+        try:
+            f = open('inputPS21.txt','r')
+        except:
+            print("File inputPS21.txt doesnot exist")
+        recordCount = 0
+        arrayOfDictPerson = []
+        arrayOfBirth = []
+        arrayOfDeath = []
+        global leastBirthYear 
+        leastBirthYear = 2100
+        global maxDeathYear
+        maxDeathYear = 0
+        for line in f:
+            recordCount+=1
+            line = line.strip()
+            rowSplit = line.split(',')
+            person = dictPerson(rowSplit[0],rowSplit[1],rowSplit[2],rowSplit[3])
+            arrayOfDictPerson.append(person)
+            birthFlag = 0
+            for birthObj in arrayOfBirth:
+                if birthObj.year == rowSplit[2].split('-')[2]:
+                    birthObj.birthCount += 1
+                    birthFlag = 1
 
-                if int(birthObj.year) < int(leastBirthYear):
-                    leastBirthYear = int(birthObj.year)
-        
-        if birthFlag == 0:
-            yB = yearBirth(rowSplit[2].split('-')[2])
-            arrayOfBirth.append(yB)
-
-        if(rowSplit[3] != ''):
-            deathFlag = 0
-            dodYear = rowSplit[3].split('-')[2]
-            for deathObj in arrayOfDeath:
-                if deathObj.year == dodYear:
-                    deathObj.deathCount += 1
-                    deathFlag = 1
-                    if int(dodYear) > int(maxDeathYear):
-                        maxDeathYear = int(dodYear)
+                    if int(birthObj.year) < int(leastBirthYear):
+                        leastBirthYear = int(birthObj.year)
             
-            if deathFlag == 0:
-                yD = yearDeath(dodYear)  #split is used to fetch only the year from the date
-                arrayOfDeath.append(yD)
-    
-    return [arrayOfDictPerson, arrayOfBirth, arrayOfDeath, recordCount ]
+            if birthFlag == 0:
+                yB = yearBirth(rowSplit[2].split('-')[2])
+                arrayOfBirth.append(yB)
+
+            if(rowSplit[3] != ''):
+                deathFlag = 0
+                dodYear = rowSplit[3].split('-')[2]
+                for deathObj in arrayOfDeath:
+                    if deathObj.year == dodYear:
+                        deathObj.deathCount += 1
+                        deathFlag = 1
+                        if int(dodYear) > int(maxDeathYear):
+                            maxDeathYear = int(dodYear)
+                
+                if deathFlag == 0:
+                    yD = yearDeath(dodYear)
+                    arrayOfDeath.append(yD)
+        
+        return [arrayOfDictPerson, arrayOfBirth, arrayOfDeath, recordCount ]
+    except:
+        print("Something went wrong while reading inputs")
 
 
 printOutput(readInputData())
